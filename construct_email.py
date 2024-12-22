d17eda6341891cdb2921a9f885dfb8d099bde39c
Ziyu Wang <96973724+yuz1wan@users.noreply.github.com>
2024-12-23 01:44:31 +0800

    Update construct_email.py
    Add html url

--- construct_email.py
+++ construct_email.py
@@ -38,8 +38,9 @@ To unsubscribe, remove your email in your Github Action setting.
 </html>
 """
 
+
 def get_empty_html():
-  block_template = """
+    block_template = """
   <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
   <tr>
     <td style="font-size: 20px; font-weight: bold; color: #333;">
@@ -47,10 +48,12 @@ def get_empty_html():
     </td>
   </tr>
   """
-  return block_template
+    return block_template
 
-def get_block_html(title:str, authors:str, rate:str,arxiv_id:str, abstract:str, pdf_url:str, code_url:str=None):
+
+def get_block_html(title: str, authors: str, rate: str, arxiv_id: str, abstract: str, pdf_url: str, code_url: str = None):
     code = f'<a href="{code_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #5bc0de; padding: 8px 16px; border-radius: 4px; margin-left: 8px;">Code</a>' if code_url else ''
+    html_url = pdf_url.replace('pdf', 'html')
     block_template = """
     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
     <tr>
@@ -82,14 +85,16 @@ def get_block_html(title:str, authors:str, rate:str,arxiv_id:str, abstract:str, 
     <tr>
         <td style="padding: 8px 0;">
             <a href="{pdf_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #d9534f; padding: 8px 16px; border-radius: 4px;">PDF</a>
+            <a href="{html_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #5bc0de; padding: 8px 16px; border-radius: 4px; margin-left: 8px;">HTML</a>
             {code}
         </td>
     </tr>
 </table>
 """
-    return block_template.format(title=title, authors=authors,rate=rate,arxiv_id=arxiv_id, abstract=abstract, pdf_url=pdf_url, code=code)
+    return block_template.format(title=title, authors=authors, rate=rate, arxiv_id=arxiv_id, abstract=abstract, pdf_url=pdf_url, html_url=html_url, code=code)
 
-def get_stars(score:float):
+
+def get_stars(score: float):
     full_star = '<span class="full-star">⭐</span>'
     half_star = '<span class="half-star">⭐</span>'
     low = 6
@@ -106,11 +111,11 @@ def get_stars(score:float):
         return '<div class="star-wrapper">'+full_star * full_star_num + half_star * half_star_num + '</div>'
 
 
-def render_email(papers:list[arxiv.Result]):
+def render_email(papers: list[arxiv.Result]):
     parts = []
-    if len(papers) == 0 :
+    if len(papers) == 0:
         return framework.replace('__CONTENT__', get_empty_html())
-    
+
     for p in papers:
         # crop the abstract
         '''
@@ -124,7 +129,8 @@ def render_email(papers:list[arxiv.Result]):
         authors = ', '.join(authors)
         if len(p.authors) > 5:
             authors += ', ...'
-        parts.append(get_block_html(p.title, authors,rate,p.arxiv_id ,p.tldr, p.pdf_url, p.code_url))
+        parts.append(get_block_html(p.title, authors, rate,
+                     p.arxiv_id, p.tldr, p.pdf_url, p.code_url))
 
     content = '<br>' + '</br><br>'.join(parts) + '</br>'
     return framework.replace('__CONTENT__', content)
